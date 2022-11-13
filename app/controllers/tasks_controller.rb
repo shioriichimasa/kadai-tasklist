3fcaf2f701aa3df80a589deaaaf98681e2a81b3d
shio.tktk <shiii000203@gmail.com>
2022-11-13 14:26:55 +0000

    final

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:show]
   
   def index
     @tasks = Task.all
@@ -55,4 +56,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
